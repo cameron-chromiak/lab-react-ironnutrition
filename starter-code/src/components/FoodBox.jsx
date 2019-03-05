f1e42554a4ca0e20d1c4f4dac51e6a79d7b972f7
cameron-chromiak <cam.chromiak@hotmail.com>
2019-03-05 10:42:47 -0500

    ITERATION one complete

---
+++ starter-code/src/components/FoodBox.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import foods from '../foods.json'
+
+
+class FoodBox extends Component {
+ state = {
+    foods: foods
+
+ }
+
+ showFoods = () => {
+   let listItems = this.state.foods.map((item,i) => {
+     return (
+       <div className="box">
+         <article className="media">
+           <div className="media-left">
+             <figure className="image is-64x64">
+               <img src="https://i.imgur.com/eTmWoAN.png" />
+             </figure>
+           </div>
+           <div className="media-content">
+             <div className="content">
+               <p>
+                 <strong>{item.name}</strong> <br />
+                 <small></small>
+               </p>
+             </div>
+           </div>
+           <div className="media-right">
+             <div className="field has-addons">
+               <div className="control">
+                 <input
+                   className="input"
+                   type="number"
+                   value="1"
+                 />
+               </div>
+               <div className="control">
+                 <button className="button is-info">
+                   +
+                 </button>
+               </div>
+             </div>
+           </div>
+         </article>
+       </div>
+      )
+      }
+     )
+
+
+   return listItems
+ }
+
+  render() {
+    return (
+      <ul>{this.showFoods()}</ul>
+    );
+  }
+}
+
+export default FoodBox;
